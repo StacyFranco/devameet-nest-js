690ae056b0b4e0741559d32677873fe9cab150e6
StacyFranco <stacyfranco@hotmail.com>
2023-11-02 17:32:33 -0300

    deeleting meeting positions on deleting meet

--- src/meet/meet.module.ts
+++ src/meet/meet.module.ts
@@ -1,18 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { MongooseModule } from "@nestjs/mongoose";
 import { MeetController } from './meet.controller';
 import { MeetService } from './meet.service';
 import { UserModule } from 'src/user/user.module';
-import { Meet,MeetSchema } from './schemas/meet.schema';
+import { Meet, MeetSchema } from './schemas/meet.schema';
 import { MeetObject, MeetObjectSchema } from './schemas/meetObject.schema';
+import { RoomModule } from 'src/room/room.module';
+import { Position, PositionSchema } from 'src/room/schemas/position.schema';
 
 @Module({
   imports: [UserModule, MongooseModule.forFeature([
-    {name: Meet.name, schema: MeetSchema},
-    {name: MeetObject.name, schema: MeetObjectSchema},
+    { name: Meet.name, schema: MeetSchema },
+    { name: MeetObject.name, schema: MeetObjectSchema },
+    { name: Position.name, schema: PositionSchema },
   ])],
   controllers: [MeetController],
   providers: [MeetService],
   exports: [MongooseModule, MeetService]
 })
-export class MeetModule {}
+export class MeetModule { }
