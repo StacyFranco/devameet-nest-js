39283481d5ec713bd75ffdf1ea693de41e5996d6
StacyFranco <stacyfranco@hotmail.com>
2024-04-11 21:27:57 -0300

    test cors websocket

--- src/room/room.gateway.ts
+++ src/room/room.gateway.ts
@@ -13,7 +13,12 @@ type ActiveSocketType = {
   userId: string;
 }
 
-@WebSocketGateway({ cors: true })
+@WebSocketGateway({ cors: {
+  origin: ["http://localhost:5173"],
+  credentials: true,
+},
+transports: ['websocket', 'polling'],
+} )
 export class RoomGateway implements OnGatewayInit, OnGatewayDisconnect {
 
   constructor(private readonly service: RoomService) { }
